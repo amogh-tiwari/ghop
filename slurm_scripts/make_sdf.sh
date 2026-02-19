f4559682089ab6853d6930921d56dd4c3e364d18
Amogh Tiwari <amogh.tiwari@inria.fr>
2026-02-19 12:07:54 +0100

    corresponding edits to make sdf slurm script

--- slurm_scripts/make_sdf.sh
+++ slurm_scripts/make_sdf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=TravailGPU                   # name of job
-#SBATCH --output=TravailGPU%j.out               # output file (%j = job ID)
-#SBATCH --error=TravailGPU%j.err                # error file (%j = job ID)
+#SBATCH --output=TravailGPU_%A_%a.out               # output file (%j = job ID)
+#SBATCH --error=TravailGPU_%A_%a.err                # error file (%j = job ID)
 #SBATCH --constraint=v100-16g                   # reserve GPUs with 16 GB of RAM
 #SBATCH --nodes=1                               # reserve 1 node
 #SBATCH --ntasks=1                              # reserve 4 tasks (or processes)
 #SBATCH --gres=gpu:1                            # reserve 4 GPUs
 #SBATCH --cpus-per-task=6                       # reserve 10 CPUs per task (and associated memory)
-#SBATCH --time=96:00:00                         # maximum allocation time "(HH:MM:SS)"
+#SBATCH --time=08:00:00                         # maximum allocation time "(HH:MM:SS)"
 #SBATCH --qos=qos_gpu-t4                       # QoS
 #SBATCH --hint=nomultithread                    # deactivate hyperthreading
 #SBATCH --account=tuy@v100                      # V100 accounting
-#SBATCH --array=0-99
+#SBATCH --array=0-49
 
 module purge                                    # purge modules inherited by default
 conda deactivate                                # deactivate environments inherited by default
@@ -25,7 +25,7 @@ set -x                                          # activate echo of launched comm
 
 echo "$SLURM_ARRAY_TASK_ID"
 
-chunk_size=2
+chunk_size=100
 start=$(( SLURM_ARRAY_TASK_ID * chunk_size ))
 end=$(( start + chunk_size ))
 
@@ -33,5 +33,5 @@ echo "Job $SLURM_ARRAY_TASK_ID: range [$start, $end]"
 
 export PYOPENGL_PLATFORM=egl
 # srun python -m preprocess.make_grasp_grab --start-idx 1 --end-idx 2 # execute script
-srun python -m preprocess.make_grasp_grab --start-idx $start --end-idx $end # execute script
+srun python -m preprocess.make_grasp --start_idx $start --end_idx $end # execute script
 
